35d8bf93d69a40e10f4e2acf532a25b25191daba
Yswag <91155348+Yswag@users.noreply.github.com>
2023-02-17 09:13:54 +0800

    bilibili-json.js

--- js/bilibili-json.js
+++ js/bilibili-json.js
@@ -34,51 +34,30 @@ if (body) {
             break;
         case /^https?:\/\/app\.bilibili\.com\/x\/resource\/show\/tab/.test($request.url):
             try {
-                let p = new Set([177, 178, 179, 181, 102, 104, 106, 486, 488, 489]),
-                    c = JSON.parse(body);
-                if (c.data?.tab && (c.data.tab = [{
-                        id: 39,
-                        name: "直播",
-                        uri: "bilibili://live/home",
-                        tab_id: "直播tab",
-                        pos: 1
-                    }, {
-                        id: 40,
-                        name: "推荐",
-                        uri: "bilibili://pegasus/promo",
-                        tab_id: "推荐tab",
-                        pos: 2,
-                        default_selected: 1
-                    }, {
-                        id: 41,
-                        name: "热门",
-                        uri: "bilibili://pegasus/hottopic",
-                        tab_id: "hottopic",
-                        pos: 3
-                    }, {
-                        id: 545,
-                        name: "番剧",
-                        uri: "bilibili://pgc/home",
-                        tab_id: "bangumi",
-                        pos: 4
-                    }, {
-                        id: 151,
-                        name: "影视",
-                        uri: "bilibili://pgc/cinema-tab",
-                        tab_id: "film",
-                        pos: 5
-                    }]), c.data.top && (c.data.top = [{
-                        id: 481,
-                        icon: "http://i0.hdslb.com/bfs/archive/d43047538e72c9ed8fd8e4e34415fbe3a4f632cb.png",
-                        name: "消息",
-                        uri: "bilibili://link/im_home",
-                        tab_id: "消息Top",
-                        pos: 1
-                    }]), c.data.bottom) {
-                    let n = c.data.bottom.filter(t => p.has(t.id));
-                    c.data.bottom = n
-                }
-                body = JSON.stringify(c)
+          const tabList = new Set([39, 40, 41, 545, 151, 442, 99, 100, 101, 554, 556]);
+          const tabNameList = new Set(["直播", "推薦", "IVE", "LESSERAFIM", "韓綜", "動畫"]);
+          const topList = new Set([176]);
+          const bottomList = new Set([177, 178, 179, 181, 690, 102, 103, 104, 105, 106]);
+          let obj = JSON.parse(body);
+          if (obj["data"]["tab"]) {
+            let tab = obj["data"]["tab"].filter((e) => {
+              return tabNameList.has(e.name) || tabList.has(e.id);
+            });
+            obj["data"]["tab"] = tab;
+          }
+          if (obj["data"]["top"]) {
+            let top = obj["data"]["top"].filter((e) => {
+              return topList.has(e.id);
+            });
+            obj["data"]["top"] = top;
+          }
+          if (obj["data"]["bottom"]) {
+            let bottom = obj["data"]["bottom"].filter((e) => {
+              return bottomList.has(e.id);
+            });
+            obj["data"]["bottom"] = bottom;
+          }
+          body = JSON.stringify(obj);
             } catch (y) {
                 console.log("bilibili tab processing:" + y)
             }
@@ -130,11 +109,7 @@ if (body) {
         case /^https?:\/\/app\.bilibili\.com\/x\/v2\/search\/square/.test($request.url):
             try {
                 let k = JSON.parse(body);
-                k.data = {
-                    type: "history",
-                    title: "搜索历史",
-                    search_hotword_revision: 2
-                }, body = JSON.stringify(k)
+                k.data = k.data.filter((n) => n.type == "history"), body = JSON.stringify(k)
             } catch (w) {
                 console.log("bilibili hot search:" + w)
             }
