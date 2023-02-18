28989f14520d69d7908630d3d6a31a3b14e00a48
Yswag <91155348+Yswag@users.noreply.github.com>
2023-02-19 04:42:28 +0800

    1

--- js/reddit-ad.js
+++ js/reddit-ad.js
@@ -1,15 +1,15 @@
 if ($response.body.includes("adEvents")) {
   let body = JSON.parse($response.body);
-  switch (true) {
-    case body.data.subredditInfoByName:
-      body.data.subredditInfoByName.elements.edges = body.data.subredditInfoByName.elements.edges.filter((n) => !n.node.adEvents);
-      break;
-    case body.data.home:
-      body.data.home.elements.edges = body.data.home.elements.edges.filter((n) => !n.node.adEvents);
-      body.data.home.elements.edges = body.data.home.elements.edges.filter((n) => n.node.__typename !== "PostRecommendation");
-      break;
+  if (body.data.subredditInfoByName) {
+    body.data.subredditInfoByName.elements.edges = body.data.subredditInfoByName.elements.edges.filter((n) => !!!n.node.adEvents);
+    $done({ body: JSON.stringify(body) });
+  } else if (body.data.home) {
+    body.data.home.elements.edges = body.data.home.elements.edges.filter((n) => !!!n.node.adEvents);
+		body.data.home.elements.edges = body.data.home.elements.edges.filter((n) => n.node.__typename !== "PostRecommendation");
+    $done({ body: JSON.stringify(body) });
+  } else {
+    $done({});
   }
-  $done({ body: JSON.stringify(body) });
 } else {
   $done({});
 }
