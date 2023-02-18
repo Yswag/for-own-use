acac91122d8c9670c6d96d0faa46d23b47ce3016
Yswag <91155348+Yswag@users.noreply.github.com>
2023-02-19 04:52:58 +0800

    2

--- js/reddit-ad.js
+++ js/reddit-ad.js
@@ -1,11 +1,10 @@
 if ($response.body.includes("adEvents")) {
   let body = JSON.parse($response.body);
   if (body.data.subredditInfoByName) {
-    body.data.subredditInfoByName.elements.edges = body.data.subredditInfoByName.elements.edges.filter((n) => !!!n.node.adEvents);
+    body.data.subredditInfoByName.elements.edges = body.data.subredditInfoByName.elements.edges.filter((n) => !n.node.adEvents && n.node.__typename !== "PostRecommendation");
     $done({ body: JSON.stringify(body) });
   } else if (body.data.home) {
-    body.data.home.elements.edges = body.data.home.elements.edges.filter((n) => !!!n.node.adEvents);
-		body.data.home.elements.edges = body.data.home.elements.edges.filter((n) => n.node.__typename !== "PostRecommendation");
+    body.data.home.elements.edges = body.data.home.elements.edges.filter((n) => !n.node.adEvents && n.node.__typename !== "PostRecommendation");
     $done({ body: JSON.stringify(body) });
   } else {
     $done({});
