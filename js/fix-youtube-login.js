d21caf6760c7fa273f0cedd3d6f28c0ba9f362b1
Yswag <91155348+Yswag@users.noreply.github.com>
2023-03-11 17:32:01 +0800

    Update fix-youtube-login.js

--- js/fix-youtube-login.js
+++ js/fix-youtube-login.js
@@ -1,16 +1,16 @@
 function removeUrlParameter(url,parameter) {
-	var urlParts=url.split('?');
-	if(urlParts.length>=2) {
-		var urlBase=urlParts.shift();
-		var queryString=urlParts.join('?');
-		var prefix=encodeURIComponent(parameter)+'=';
-		var parts=queryString.split(/[&;]/g);
-		for(var i=parts.length;i-->0;) {
-			if(parts[i].lastIndexOf(prefix,0)!==-1) {
+	var urlParts = url.split('?');
+	if(urlParts.length >= 2) {
+		var urlBase = urlParts.shift();
+		var queryString = urlParts.join('?');
+		var prefix = encodeURIComponent(parameter) + '=';
+		var parts = queryString.split(/[&;]/g);
+		for(var i = parts.length;i-->0;) {
+			if(parts[i].lastIndexOf(prefix,0) !==-1) {
 				parts.splice(i,1);
 			}
 		}
-		url=urlBase+'?'+parts.join('&');
+		url = urlBase + '?' + parts.join('&');
 	}
 	return url;
 }
