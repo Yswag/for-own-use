c2765545770cefb59ca88248da67882034b2a5d6
Yswag <91155348+Yswag@users.noreply.github.com>
2023-07-19 10:07:39 +0800

    statfm

---
+++ js/statsfm.js
@@ -0,0 +1,10 @@
+let data = JSON.parse($response.body);
+data.item.isPro = true;
+data.item.isPlus = true;
+data.item.hasSwipefy = true;
+data.item.displayName = "iwannaknow你行不行";
+data.item.customId = "kimgiwon_04";
+#data.item.profile.bio = "";
+#data.item.profile.theme = "pink";
+data.item.image = "https:\/\/media.discordapp.net\/attachments\/1052344286676529152\/1100829429749530694\/b99069341754db48.png";
+$done({ body: JSON.stringify(data) });
