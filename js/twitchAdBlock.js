15416b1f55791317f3d5bf38a367ab3b71e63165
Yswag <91155348+Yswag@users.noreply.github.com>
2023-10-06 06:02:27 +0800

    Update twitchAdBlock.js
    支持vod

--- js/twitchAdBlock.js
+++ js/twitchAdBlock.js
@@ -1,49 +1,54 @@
 const url = $request.url;
-const newURL = removeUrlParameter(removeUrlParameter(url, "token"), "sig"); // 移除網址中的敏感資訊
-// ttv lol 的 api 需要 Twitch token 才能看 vod
-const playlist = newURL.slice("https://usher.ttvnw.net/api/channel/hls/".length);
+const newURL = removeUrlParameters(url, ["token", "sig", "p", "play_session_id"]); // 移除網址中的敏感資訊
 
-$httpClient.get("https://eu.luminous.dev/ping", function (error, response, data) {
-	if (response.status != 200) {
-		$notification.post("Twitch去廣告", "", "伺服器異常，嘗試eu2.luminous.dev...");
-		$httpClient.get("https://eu2.luminous.dev/ping", function (error, response, data) {
-			if (response.status != 200) {
-				$notification.post("Twitch去廣告", "", "luminous.dev伺服器異常，返回原始連結");
-				$done({});
-			} else if (response.status === 200) {
-				let proxy = "https://eu2.luminous.dev/playlist/" + encodeURIComponent(playlist);
-				$done({
-					status: 302,
-					headers: {
-						Location: proxy,
-					},
-				});
-			}
-		});
-	} else if (response.status === 200) {
-		let proxy = "https://eu.luminous.dev/playlist/" + encodeURIComponent(playlist);
-		$done({
-			status: 302,
-			headers: {
-				Location: proxy,
-			},
-		});
-	}
-});
-
-function removeUrlParameter(url, parameter) {
-	let urlParts = url.split("?");
-	if (urlParts.length >= 2) {
-		let urlBase = urlParts.shift();
-		let queryString = urlParts.join("?");
-		let prefix = encodeURIComponent(parameter) + "=";
-		let parts = queryString.split(/[&;]/g);
-		for (let i = parts.length; i-- > 0; ) {
-			if (parts[i].lastIndexOf(prefix, 0) !== -1) {
-				parts.splice(i, 1);
-			}
+if (url.includes("https://usher.ttvnw.net/api/channel/hls")) {
+	const live = newURL.slice("https://usher.ttvnw.net/api/channel/hls/".length);
+	$httpClient.get("https://eu.luminous.dev/ping", function (error,response) {
+		if (response.status != 200) {
+			$done({});
+		} else {
+			const proxy = "https://eu.luminous.dev/live/" + encodeURIComponent(live);
+			$done({
+				status: 302,
+				headers: {
+					Location: proxy,
+				},
+			});
 		}
-		url = urlBase + "?" + parts.join("&");
-	}
-	return url;
+	});
+} else if (url.includes("https://usher.ttvnw.net/vod")) {
+	const vod = newURL.slice("https://usher.ttvnw.net/vod/".length);
+	$httpClient.get("https://eu.luminous.dev/ping", function (error,response) {
+		if (response.status != 200) {
+			$done({});
+		} else {
+			const proxy = "https://eu.luminous.dev/vod/" + encodeURIComponent(vod);
+			$done({
+				status: 302,
+				headers: {
+					Location: proxy,
+				},
+			});
+		}
+	});
+}
+
+function removeUrlParameters(url, parameters) {
+  let urlParts = url.split("?");
+  if (urlParts.length >= 2) {
+    let urlBase = urlParts.shift();
+    let queryString = urlParts.join("?");
+    let parts = queryString.split(/[&;]/g);
+    for (let i = parts.length; i-- > 0; ) {
+      for (let j = 0; j < parameters.length; j++) {
+        let prefix = encodeURIComponent(parameters[j]) + "=";
+        if (parts[i].lastIndexOf(prefix, 0) !== -1) {
+          parts.splice(i, 1);
+          break;
+        }
+      }
+    }
+    url = urlBase + "?" + parts.join("&");
+  }
+  return url;
 }
