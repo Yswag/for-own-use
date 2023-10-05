4a2f11839b19980cf80f743a1ee7a86c911a3ed5
Yswag <91155348+Yswag@users.noreply.github.com>
2023-10-06 06:14:19 +0800

    Fix formatting issues and update Twitch ad block script

--- js/twitchAdBlock.js
+++ js/twitchAdBlock.js
@@ -3,11 +3,11 @@ const newURL = removeUrlParameters(url, ["token", "sig", "p", "play_session_id"]
 
 if (url.includes("https://usher.ttvnw.net/api/channel/hls")) {
 	const live = newURL.slice("https://usher.ttvnw.net/api/channel/hls/".length);
-	$httpClient.get("https://eu.luminous.dev/ping", function (error,response) {
+	$httpClient.get("https://eu.luminous.dev/ping", function (error, response) {
 		if (response.status != 200) {
 			$done({});
 		} else {
-			const proxy = "https://eu.luminous.dev/live/" + encodeURIComponent(live);
+			const proxy = `https://eu.luminous.dev/live/${encodeURIComponent(live)}`;
 			$done({
 				status: 302,
 				headers: {
@@ -18,11 +18,11 @@ if (url.includes("https://usher.ttvnw.net/api/channel/hls")) {
 	});
 } else if (url.includes("https://usher.ttvnw.net/vod")) {
 	const vod = newURL.slice("https://usher.ttvnw.net/vod/".length);
-	$httpClient.get("https://eu.luminous.dev/ping", function (error,response) {
+	$httpClient.get("https://eu.luminous.dev/ping", function (error, response) {
 		if (response.status != 200) {
 			$done({});
 		} else {
-			const proxy = "https://eu.luminous.dev/vod/" + encodeURIComponent(vod);
+			const proxy = `https://eu.luminous.dev/vod/${encodeURIComponent(vod)}`;
 			$done({
 				status: 302,
 				headers: {
@@ -34,21 +34,22 @@ if (url.includes("https://usher.ttvnw.net/api/channel/hls")) {
 }
 
 function removeUrlParameters(url, parameters) {
-  let urlParts = url.split("?");
-  if (urlParts.length >= 2) {
-    let urlBase = urlParts.shift();
-    let queryString = urlParts.join("?");
-    let parts = queryString.split(/[&;]/g);
-    for (let i = parts.length; i-- > 0; ) {
-      for (let j = 0; j < parameters.length; j++) {
-        let prefix = encodeURIComponent(parameters[j]) + "=";
-        if (parts[i].lastIndexOf(prefix, 0) !== -1) {
-          parts.splice(i, 1);
-          break;
-        }
-      }
-    }
-    url = urlBase + "?" + parts.join("&");
-  }
-  return url;
+	let urlParts = url.split("?");
+	if (urlParts.length >= 2) {
+		let urlBase = urlParts.shift();
+		let queryString = urlParts.join("?");
+		let parts = queryString.split(/[&;]/g);
+		parts = parts.filter((part) => {
+			for (let j = 0; j < parameters.length; j++) {
+				const parameter = parameters[j];
+				let prefix = encodeURIComponent(parameter) + "=";
+				if (part.lastIndexOf(prefix, 0) === 0) {
+					return false;
+				}
+			}
+			return true;
+		});
+		url = urlBase + "?" + parts.join("&");
+	}
+	return url;
 }
