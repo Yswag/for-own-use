a50a476463e4c5b87d4580cd0eefc004da327c8f
Yswag <91155348+Yswag@users.noreply.github.com>
2024-03-28 00:18:05 +0800

    update cmsAdblock

--- js/cmsAdblock.js
+++ js/cmsAdblock.js
@@ -19,29 +19,25 @@ const haiwaikan = [
 	":3.136466,",
 	":3.103100,",
 	":3.10,",
-	":3,",
 	":2.936266,",
 	":2.602600,",
 	":2.235567,",
 	":2.002000,",
 	":2.00,",
-	":2,",
 	":1.968633,",
 	":1.96,",
 	":1.36,",
 	":1.334666,",
 	":1.768432,",
 	":1.368033,",
-	":1,",
 	":0.266932,",
 	":0.26,",
-	":0,",
 ];
 
 const lzzy = [
 	":7.166667,",
 	":7.041667,",
-	":4.800000,",
+	//":4.800000,",
 	":4.166667,",
 	":2.833333,",
 	":2.733333,",
@@ -58,41 +54,69 @@ const ffzy = [
 
 const url = $request.url;
 const lines = $response.body.split("\n");
-let valuesToRemove = [];
+
 let indexesToRemove = [];
 let website = "";
+let adCount = 0;
 
 switch (true) {
 	case url.includes("v.cdnlz"):
 	case url.includes("lz-cdn"):
-		valuesToRemove = lzzy;
+		filterAds(lzzy);
+		removeAds();
 		website = "量子資源";
 		break;
 	case url.includes("m3u.haiwaikan"):
-		valuesToRemove = haiwaikan;
+		haiwaikanHostsCount();
+		filterAds(haiwaikan);
+		removeAds();
 		website = "海外看";
 		break;
 	case url.includes("ffzy"):
-		valuesToRemove = ffzy;
+		filterAds(ffzy);
+		removeAds();
 		website = "非凡資源";
 		break;
 	default:
 		break;
 }
 
-for (let i = lines.length - 1; i >= 0; i--) {
-	if (valuesToRemove.some((value) => lines[i].includes(value))) {
-		indexesToRemove.push(i);
+function filterAds(valuesToRemove) {
+	for (let i = lines.length - 1; i >= 0; i--) {
+		if (valuesToRemove.some((value) => lines[i].includes(value))) {
+			indexesToRemove.push(i);
+		}
 	}
 }
 
-let count = 0;
-indexesToRemove.forEach((indexToRemove) => {
-	if (indexToRemove !== -1 && lines[indexToRemove + 1].endsWith(".ts")) {
-		lines.splice(indexToRemove, 2);
-		count++;
-	}
-});
+function removeAds() {
+	indexesToRemove.forEach((indexToRemove) => {
+		if (indexToRemove !== -1 && lines[indexToRemove].endsWith(".ts")) {
+			//hostname
+			lines.splice(indexToRemove - 1, 2);
+			adCount++;
+		} else if (indexToRemove !== -1 && lines[indexToRemove + 1].endsWith(".ts")) {
+			//duration
+			lines.splice(indexToRemove, 2);
+			adCount++;
+		}
+	});
+}
 
-console.log(`移除${website}廣告${count}行`);
+function haiwaikanHostsCount() {
+	const hostsCount = {};
+	lines.forEach((line) => {
+		if (line.includes(".ts")) {
+			const hostname = new URL(line).hostname;
+			hostsCount[hostname] = (hostsCount[hostname] || 0) + 1;
+		}
+	});
+
+	const keys = Object.keys(hostsCount);
+	if (keys.length >= 2) {
+		haiwaikan.push(keys[1]);
+	} else return;
+}
+
+console.log(`移除${website}廣告${adCount}行`);
 $done({ body: lines.join("\n") });
