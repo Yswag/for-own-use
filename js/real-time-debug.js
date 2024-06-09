9641f50857f45af3dc9e5bd58d24fd3bf8c92bdb
Yswag <91155348+Yswag@users.noreply.github.com>
2024-06-09 19:13:23 +0800

    Update real-time-debug.js

--- js/real-time-debug.js
+++ js/real-time-debug.js
@@ -9,7 +9,7 @@
 	const _$ = new nobyda();
 	const _r = await new Promise(e => {
 		_$.get({
-			url: 'http://192.168.1.66:5500/debug.js' // LAN backend address
+			url: 'http://192.168.50.136:5500/debug.js' // LAN backend address
 		}, (t, c, o) => {
 			if (c && c.status == 200 && o) {
 				_$.write(o, 'Real-time-debug');
