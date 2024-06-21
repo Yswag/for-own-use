8283531f505e460ed00f82c9096d25e6fea532c2
Yswag <91155348+Yswag@users.noreply.github.com>
2024-06-21 10:34:05 +0800

    Update ofiii.js

--- js/ofiii.js
+++ js/ofiii.js
@@ -3,17 +3,22 @@ if ($response.body === undefined) $done({})
 
 let body = JSON.parse($response.body)
 
-body.section.forEach((e) => {
-	// Banner輪播
-	if (e.sectionId === '1') {
-		let banners = e.item
-		let cleanItem = []
-		banners.forEach((e) => {
-			if (e.isVod === true) cleanItem.push(e)
-		})
+// 直播頻道不修改
+if (body.id === '2') $done({})
 
-		e.item = cleanItem
-	}
-})
+body.section[0].item = body.section[0].item.filter((e) => e.isVod === true)
+
+//body.section.forEach((e) => {
+// Banner輪播
+//	if (e.title.includes('輪播')) {
+//		let banners = e.item
+//		let cleanItem = []
+//		banners.forEach((e) => {
+//			if (e.isVod === true) cleanItem.push(e)
+//		})
+
+//		e.item = cleanItem
+//	}
+//})
 
 $done({ body: JSON.stringify(body) })
