81d442a93bca178da6dad29607213faf70babf25
Yswag <91155348+Yswag@users.noreply.github.com>
2024-06-26 04:50:40 +0800

    Update cmsAdblock

--- js/cmsAdblock.js
+++ js/cmsAdblock.js
@@ -82,6 +82,13 @@ let t097img = ['y.ts']
 
 let temp = []
 
+let arg = getArg()
+
+if (arg === '2') {
+	$.log('using 2nd method')
+	fixAdM3u8Ai($request.url)
+}
+
 if ($response.body === undefined || !$response.body.includes('#EXTM3U')) $.done({})
 
 const url = $request.url
@@ -253,8 +260,8 @@ async function fetchJxResult() {
 			$.log(resp.body)
 			if (body.url !== $request.url) {
 				const content = await $.http.get(body.url)
-				$.log('Modified by '+ body.url)
-				$.done({body:content.body})
+				$.log('Modified by ' + body.url)
+				$.done({ body: content.body })
 			} else return
 		} catch (e) {
 			$.log(e)
@@ -289,6 +296,134 @@ async function fetchJxResult() {
 		$.log(e)
 		$.done({})
 	}
+}
+
+function getArg() {
+	if (typeof $argument === 'undefined') {
+		return '1'
+	}
+	return $argument
+			
+}
+
+function fixAdM3u8Ai(m3u8_url, headers) {
+	let ts = new Date().getTime()
+	let option = headers ? { headers: headers } : {}
+
+	function b(s1, s2) {
+		let i = 0
+		while (i < s1.length) {
+			if (s1[i] !== s2[i]) {
+				break
+			}
+			i++
+		}
+		return i
+	}
+
+	function reverseString(str) {
+		return str.split('').reverse().join('')
+	}
+
+	//log('播放的地址：' + m3u8_url);
+	let m3u8 = $response.body
+	//log('m3u8处理前:' + m3u8);
+	//m3u8 = m3u8
+	//	.trim()
+	//	.split('\n')
+	//	.map((it) => (it.startsWith('#') ? it : urljoin(m3u8_url, it)))
+	//	.join('\n')
+	//log('m3u8处理后:============:' + m3u8);
+	// 获取嵌套m3u8地址
+	//m3u8 = m3u8.replace(/\n\n/gi, '\n') //删除多余的换行符
+	//let last_url = m3u8.split('\n').slice(-1)[0]
+	//if (last_url.length < 5) {
+	//	last_url = m3u8.split('\n').slice(-2)[0]
+	//}
+
+	//if (last_url.includes('.m3u8') && last_url !== m3u8_url) {
+	//	m3u8_url = urljoin2(m3u8_url, last_url)
+	//	$.log('嵌套的m3u8_url:' + m3u8_url)
+	//	m3u8 = request(m3u8_url, option)
+	//}
+	//log('----处理有广告的地址----');
+	let s = m3u8
+		.trim()
+		.split('\n')
+		.filter((it) => it.trim())
+		.join('\n')
+	let ss = s.split('\n')
+	//找出第一条播放地址
+	//let firststr = ss.find(x => !x.startsWith('#'));
+	let firststr = ''
+	let maxl = 0 //最大相同字符
+	let kk = 0
+	let kkk = 2
+	let secondstr = ''
+	for (let i = 0; i < ss.length; i++) {
+		let s = ss[i]
+		if (!s.startsWith('#')) {
+			if (kk == 0) firststr = s
+			if (kk == 1) maxl = b(firststr, s)
+			if (kk > 1) {
+				if (maxl > b(firststr, s)) {
+					if (secondstr.length < 5) secondstr = s
+					kkk = kkk + 2
+				} else {
+					maxl = b(firststr, s)
+					kkk++
+				}
+			}
+			kk++
+			if (kk >= 20) break
+		}
+	}
+	if (kkk > 30) firststr = secondstr
+	let firststrlen = firststr.length
+	//log('字符串长度：' + firststrlen);
+	let ml = Math.round(ss.length / 2).toString().length //取数据的长度的位数
+	//log('数据条数的长度：' + ml);
+	//找出最后一条播放地址
+	let maxc = 0
+	let laststr = ss.toReversed().find((x) => {
+		if (!x.startsWith('#')) {
+			let k = b(reverseString(firststr), reverseString(x))
+			maxl = b(firststr, x)
+			maxc++
+			if (firststrlen - maxl <= ml + k || maxc > 10) {
+				return true
+			}
+		}
+		return false
+	})
+	$.log('最后一条切片：' + laststr)
+	//log('最小相同字符长度：' + maxl);
+	let ad_urls = []
+	for (let i = 0; i < ss.length; i++) {
+		let s = ss[i]
+		if (!s.startsWith('#')) {
+			if (b(firststr, s) < maxl) {
+				ad_urls.push(s) // 广告地址加入列表
+				ss.splice(i - 1, 2)
+				i = i - 2
+			} else {
+				//ss[i] = urljoin(m3u8_url, s)
+				ss[i] = s
+			}
+		} else {
+			//ss[i] = s.replace(/URI=\"(.*)\"/, 'URI="' + urljoin(m3u8_url, '$1') + '"')
+			ss[i] = s
+		}
+	}
+	$.log('处理的m3u8地址:' + m3u8_url)
+	$.log('----广告地址----')
+	$.log(ad_urls)
+	$.log('移除廣告' + ad_urls.length + '行')
+	m3u8 = ss.join('\n')
+	//log('处理完成');
+	$.log('处理耗时：' + (new Date().getTime() - ts).toString())
+	//return m3u8
+	$.done({ body: m3u8 })
 }
 
 //prettier-ignore
