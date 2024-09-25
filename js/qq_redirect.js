03f9d32e64cbc710f41f2b0deb02943d9ad95efa
ykusu <yangswag0607@gmail.com>
2024-09-26 00:23:57 +0800

    update

---
+++ js/qq_redirect.js
@@ -0,0 +1,114 @@
+class URL {
+    constructor(url, base = undefined) {
+        const name = 'URL'
+        const version = '2.1.2'
+        console.log(`\n🟧 ${name} v${version}\n`)
+        url = this.#parse(url, base)
+        return this
+    }
+
+    #parse(url, base = undefined) {
+        const URLRegex =
+            /(?:(?<protocol>\w+:)\/\/(?:(?<username>[^\s:"]+)(?::(?<password>[^\s:"]+))?@)?(?<host>[^\s@/]+))?(?<pathname>\/?[^\s@?]+)?(?<search>\?[^\s?]+)?/
+        const PortRegex = /(?<hostname>.+):(?<port>\d+)$/
+        url = url.match(URLRegex)?.groups || {}
+        if (base) {
+            base = base?.match(URLRegex)?.groups || {}
+            if (!base.protocol || !base.hostname) throw new Error(`🚨 ${name}, ${base} is not a valid URL`)
+        }
+        if (url.protocol || base?.protocol) this.protocol = url.protocol || base.protocol
+        if (url.username || base?.username) this.username = url.username || base.username
+        if (url.password || base?.password) this.password = url.password || base.password
+        if (url.host || base?.host) {
+            this.host = url.host || base.host
+            Object.freeze(this.host)
+            this.hostname = this.host.match(PortRegex)?.groups.hostname ?? this.host
+            this.port = this.host.match(PortRegex)?.groups.port ?? ''
+        }
+        if (url.pathname || base?.pathname) {
+            this.pathname = url.pathname || base?.pathname
+            if (!this.pathname.startsWith('/')) this.pathname = '/' + this.pathname
+            this.paths = this.pathname.split('/').filter(Boolean)
+            Object.freeze(this.paths)
+            if (this.paths) {
+                const fileName = this.paths[this.paths.length - 1]
+                if (fileName?.includes('.')) {
+                    const list = fileName.split('.')
+                    this.format = list[list.length - 1]
+                    Object.freeze(this.format)
+                }
+            }
+        } else this.pathname = ''
+        if (url.search || base?.search) {
+            this.search = url.search || base.search
+            Object.freeze(this.search)
+            if (this.search)
+                this.searchParams = this.search
+                    .slice(1)
+                    .split('&')
+                    .map((param) => param.split('='))
+        }
+        this.searchParams = new Map(this.searchParams || [])
+        this.harf = this.toString()
+        Object.freeze(this.harf)
+        return this
+    }
+
+    toString() {
+        let string = ''
+        if (this.protocol) string += this.protocol + '//'
+        if (this.username) string += this.username + (this.password ? ':' + this.password : '') + '@'
+        if (this.hostname) string += this.hostname
+        if (this.port) string += ':' + this.port
+        if (this.pathname) string += this.pathname
+        if (this.searchParams.size !== 0)
+            string +=
+                '?' +
+                Array.from(this.searchParams)
+                    .map((param) => param.join('='))
+                    .join('&')
+        return string
+    }
+
+    toJSON() {
+        return JSON.stringify({ ...this })
+    }
+}
+
+const url = new URL($request.url)
+const { host, pathname: path } = url
+
+let origin = undefined
+switch (host) {
+    case 'c.pc.qq.com':
+        switch (path) {
+            case '/middlect.html':
+            case '/middlem.html':
+            case '/index.html':
+                origin = decodeURIComponent(url.searchParams.get('pfurl'))
+                break
+            case '/ios.html':
+                origin = decodeURIComponent(url.searchParams.get('url'))
+                break
+            default:
+                console.log('Unknown')
+                break
+        }
+        break
+    case 'cgi.connect.qq.com':
+        origin = decodeURIComponent(url.searchParams.get('url'))
+        break
+    case 'pingtas.qq.com':
+        console.log('Unknown')
+        break
+    default:
+        console.log('Unknown')
+        break
+}
+
+$done({
+    ststus: 302,
+    headers: {
+        Location: origin,
+    },
+})
