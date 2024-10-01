c58ad676c5b08605b358d965a715dde85f4cdeb3
Yswag <91155348+Yswag@users.noreply.github.com>
2024-10-02 04:32:47 +0800

    Update reddit

--- js/reddit.js
+++ js/reddit.js
@@ -1,14 +1,52 @@
-var obj = JSON.parse($response.body);
-obj['has_gold_subscription'] = true;
-obj['pref_autoplay'] = false;
-obj['has_subscribed_to_premium'] = true;
-obj['has_visited_new_profile'] = true;
-obj['pref_video_autoplay'] = false;
-obj['features']['promoted_trend_blanks'] = false;
-obj['is_mod'] = true;
-obj['is_gold'] = true;
-obj['has_ios_subscription'] = true;
-obj['seen_premium_adblock_modal'] = true;
-obj['can_edit_name'] = true;
-obj['has_external_account'] = true;
-$done({ body: JSON.stringify(obj) });
+let body = JSON.parse($response.body)
+
+switch (true) {
+  // home
+  case body.data.homeV3 !== undefined:
+    let homeEdges = body.data.homeV3.elements.edges
+    homeEdges = homeEdges.filter((e) => {
+      if (e.node.adPayload) {
+        console.log('Remove home feed Ads: ' + e.node.id)
+        return false
+      }
+      return true
+    })
+    body.data.homeV3.elements.edges = homeEdges
+    break
+
+  // subreddit
+  case body.data.subredditV3 !== undefined:
+    let subredditEdges = body.data.subredditV3.elements.edges
+    subredditEdges = subredditEdges.filter((e) => {
+      if (e.node.adPayload) {
+        console.log('Remove subreddit feed Ads: ' + e.node.id)
+        return false
+      }
+      if (e.node.communityRecommendations) {
+        console.log('Remove community recommendations.')
+        return false
+      }
+      return true
+    })
+    body.data.subredditV3.elements.edges = subredditEdges
+    break
+
+  // post
+  case body.data.children?.commentsPageAds !== undefined:
+    if (body.data.children.commentsPageAds.length) {
+      body.data.children.commentsPageAds = []
+      console.log('Remove comment page Ads.')
+    }
+    break
+
+  case body.data.identity !== undefined:
+    body.data.identity.redditor.profile.isSubscribed = true
+    break
+
+  default:
+    break
+}
+
+$done({
+  body: JSON.stringify(body),
+})
