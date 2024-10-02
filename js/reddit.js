bdb0579e20cabd78e6b8d9830d7c08a7e18f5be3
Yswag <91155348+Yswag@users.noreply.github.com>
2024-10-03 06:04:43 +0800

    update

--- js/reddit.js
+++ js/reddit.js
@@ -1,82 +1,23 @@
 let body = JSON.parse($response.body)
 
-switch (true) {
-  // home
-  case body.data.homeV3 !== undefined:
-    let homeV3Edges = body.data.homeV3.elements.edges
-    homeV3Edges = homeV3Edges.filter((e) => {
+for (let key in body.data) {
+  let edges = body.data[key]?.elements?.edges
+
+  if (edges) {
+    edges = edges.filter((e) => {
       if (e.node.adPayload) {
-        console.log('Remove home feed Ads: ' + e.node.id)
+        console.log('Remove feed Ads: ' + e.node.id)
         return false
       }
       return true
     })
-    body.data.homeV3.elements.edges = homeV3Edges
-    break
+    body.data[key].elements.edges = edges
+  }
+}
 
-  case body.data.home !== undefined:
-    let homeEdges = body.data.home.elements.edges
-    homeEdges = homeEdges.filter((e) => {
-      if (e.node.__typename === 'AdPost') {
-        console.log('Remove home feed Ads: ' + e.node.id)
-        return false
-      }
-      return true
-    })
-    body.data.home.elements.edges = homeEdges
-    break
-
-  case body.data.popularV3 !== undefined:
-    let popularEdges = body.data.popularV3.elements.edges
-    popularEdges = popularEdges.filter((e) => {
-      if (e.node.adPayload) {
-        console.log('Remove popular feed Ads: ' + e.node.id)
-        return false
-      }
-      return true
-    })
-    body.data.popularV3.elements.edges = popularEdges
-    break
-
-  case body.data.newsV3 !== undefined:
-    let newsEdges = body.data.newsV3.elements.edges
-    newsEdges = newsEdges.filter((e) => {
-      if (e.node.adPayload) {
-        console.log('Remove news feed Ads: ' + e.node.id)
-        return false
-      }
-      return true
-    })
-    body.data.newsV3.elements.edges = newsEdges
-    break
-
-  // subreddit
-  case body.data.subredditV3 !== undefined:
-    let subredditEdges = body.data.subredditV3.elements.edges
-    subredditEdges = subredditEdges.filter((e) => {
-      if (e.node.adPayload) {
-        console.log('Remove subreddit feed Ads: ' + e.node.id)
-        return false
-      }
-      if (e.node.communityRecommendations) {
-        console.log('Remove community recommendations.')
-        return false
-      }
-      return true
-    })
-    body.data.subredditV3.elements.edges = subredditEdges
-    break
-
-  // post
-  case body.data.children?.commentsPageAds !== undefined:
-    if (body.data.children.commentsPageAds.length) {
-      body.data.children.commentsPageAds = []
-      console.log('Remove comment page Ads.')
-    }
-    break
-
-  default:
-    break
+if (body.data?.children?.commentsPageAds?.length) {
+  body.data.children.commentsPageAds = []
+  console.log('Remove comment page Ads.')
 }
 
 $done({
